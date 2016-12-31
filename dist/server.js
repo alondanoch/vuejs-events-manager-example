3061bcb3ba261830731b94b01985002cf8a21924
alon danoch <alondanoch@hotmail.com>
2016-12-31 15:35:33 +0200

    update server

--- dist/server.js
+++ dist/server.js
@@ -22,7 +22,6 @@ app.listen(port, function () {
 
 
 var updateMeetupEvetnsRecomended = function () {
-
   fetch('https://api.meetup.com/recommended/events?photo-host=public&page=40&lon=34.855499&lat=32.109333&key=3b282661651b31583a666a6f13744a13')
     .then(function (res) {
       return res.json();
